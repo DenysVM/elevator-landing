8db27770f90e391bda79a1b12899edf723eff0fb
DenysVM <muratov.d@gmail.com>
2025-09-08 19:58:50 +0200

    ColorMode fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { createLocalStorageManager } from '@chakra-ui/react';
 import { RouterProvider } from 'react-router-dom';
 import { theme } from './theme';
 import './i18n';
 import { router } from '@/router';
 
+const colorModeManager = createLocalStorageManager('liftpro-color-mode');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <ChakraProvider theme={theme}>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+  <ChakraProvider theme={theme} colorModeManager={colorModeManager}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} storageKey="liftpro-color-mode" />
     <RouterProvider router={router} future={{ v7_startTransition: true }} />
   </ChakraProvider>
 );
