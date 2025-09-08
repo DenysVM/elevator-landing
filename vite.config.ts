02c2f7771706020bbaf55c3388c92106b63b1614
DenysVM <muratov.d@gmail.com>
2025-09-08 18:57:21 +0200

    chore: initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-const base = process.env.VITE_BASE || (process.env.NODE_ENV === 'production' ? '/elevator-landing/' : '/');
+const base = process.env.VITE_BASE || '/';
 
 export default defineConfig({
   base,
